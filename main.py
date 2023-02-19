6e845cb8a06e273c6ba993af16a5bc9d7255ebff
VA <dclass096@gmail.com>
2023-02-19 20:20:57 +0500

    1

--- main.py
+++ main.py
@@ -1,71 +1,80 @@
-import os
 import sys
+import os
 
 import requests
-from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtCore import Qt
 from PyQt5 import uic
+
+SCREEN_SIZE = [600, 450]
 
 
 class Example(QWidget):
     def __init__(self):
         super().__init__()
-        uic.loadUi('map.ui', self)
+        uic.loadUi("map.ui", self)
         self.long = 37.53
         self.lat = 55.70
         self.spn_x = 0.005
         self.spn_y = 0.005
-        self.map_type = 'map'
-        self.zoom = 5
+        self.zoom = 17
+        self.map_type = "map"
+        for b in self.buttonGroup.buttons():
+            b.clicked.connect(self.choose_type)
+
+        #self.groupBox.clicked.connect(self.choose_type)
         self.getImage()
 
-    def choose_type(self):
-        pass
-        #index = [self.groupBox.buttons()[x].isChecked() for x in ]
-        #if self.radioButton.isChecked():
-         #   self.map_type = 'map'
-         #   self.getImage()
-       # elif self.radioButton1.isChecked():
-        #    self.map_type = 'sat'
-        #    self.getImage()
-        #elif Inde:
-        #    self.map_type = 'sat,skl'
-         #   self.getImage()
+    def choose_type(self, event):
+        indexOfChecked = [self.buttonGroup.buttons()[x].isChecked() for x in
+                          range(len(self.buttonGroup.buttons()))].index(True)
+        if indexOfChecked == 0:
+            self.map_type = "map"
+            self.getImage()
+        elif indexOfChecked == 1:
+            self.map_type = "sat"
+            self.getImage()
+        elif indexOfChecked == 2:
+            self.map_type = "sat,skl"
+            self.getImage()
 
-    def KeyPressedEvent(self, event):
-        if event.key() ==Qt.Key_PageUp:
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_O:
             self.spn_x /= 2
             self.spn_y /= 2
             self.getImage()
-        elif event.key() ==Qt.Key_PageDown:
+        elif event.key() == Qt.Key_L:
             self.spn_x *= 2
             self.spn_y *= 2
             self.getImage()
-        elif event.key() ==Qt.Key_Down:
+        elif event.key() == Qt.Key_Down:
             self.lat -= self.spn_y
             self.getImage()
-        elif event.key() ==Qt.Key_Up:
+        elif event.key() == Qt.Key_Up:
             self.lat += self.spn_y
             self.getImage()
-        elif event.key() ==Qt.Key_Left:
+        elif event.key() == Qt.Key_Left:
             self.long -= self.spn_x
             self.getImage()
-        elif event.key() ==Qt.Key_Right:
+        elif event.key() == Qt.Key_Right:
             self.long += self.spn_x
             self.getImage()
 
+    # code
     def getImage(self):
+        # Собираем параметры для запроса к StaticMapsAPI:
         map_params = {
-            "ll": f'{self.long},{self.lat}',
+            "ll": f"{self.long},{self.lat}",
             "spn": f"{self.spn_x},{self.spn_y}",
+            #"zoom": self.zoom,
             "l": self.map_type
-            #'zoom': self.zoom
-            }
+        }
 
         map_api_server = "http://static-maps.yandex.ru/1.x/"
-        # ... и выполняем запрос
         response = requests.get(map_api_server, params=map_params)
+
         if not response:
             print("Ошибка выполнения запроса:")
             print("Http статус:", response.status_code, "(", response.reason, ")")
@@ -76,9 +85,7 @@ class Example(QWidget):
         with open(self.map_file, "wb") as file:
             file.write(response.content)
 
-        ## Изображение
         self.pixmap = QPixmap(self.map_file)
-        self.image = QLabel(self)
         self.label.setPixmap(self.pixmap)
 
     def closeEvent(self, event):
